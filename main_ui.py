25101baacf89f8c9bbd1916486fce3116f0afbb6
Jenifar481 <ajenifar3105@gmail.com>
2026-01-22 23:37:23 +0530

    Refactor main_ui.py for improved structure

--- main_ui.py
+++ main_ui.py
@@ -18,7 +18,7 @@ with col2:
 
 now = datetime.now()
 st.markdown(
-    f"<p style='text-align:center; color:gray;'>🗒 ⤷{now.strftime('%A, %d %B %Y')}✦</p>",
+    f"<p style='text-align:center; color:gray;'>🗒 ⤷{now.strftime('%A, %d %B %Y')} ✦</p>",
     unsafe_allow_html=True
 )
 
@@ -83,6 +83,7 @@ st.markdown(
     "<hr><center>Built with Streamlit • LangChain</center>",
     unsafe_allow_html=True
 )
+
 
 
 
