bee317ddac3a1e7a93fd818912ce54e38af61f7c
Jenifar481 <ajenifar3105@gmail.com>
2026-01-30 19:51:55 +0530

    Refactor app.py for improved structure and clarity

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from langchain_classic import hub
 load_dotenv()
 
 # ------------------ LLM ------------------
-llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite")
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 
 # ------------------ TOOLS ------------------
 wiki = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())
@@ -93,6 +93,7 @@ def run_pipeline(query: str) -> Dict[str, Any]:
         "summary": summary,
         "email": email
     }
+
 
 
 
